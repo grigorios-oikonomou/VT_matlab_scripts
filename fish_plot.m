10bc46b442dd62e2ba28bb37fe3700088622dea6
Grigorios Oikonomou <goikonomou@gmail.com>
2023-06-06 11:50:02 -0700

    Correcting for deprecation of "nanmean"

--- fish_plot.m
+++ fish_plot.m
@@ -5,7 +5,7 @@ clear lb
 lb = find(genox.lightboundries ==1)/60;
 lb(end+1) = size(genox.onehour{1},1);
 for i = 1:size(genox.data,2)
-    top(i) = max(nanmean(genox.onehour{i}'));
+    top(i) = max(mean(genox.onehour{i}', 'omitnan'));
 end
 for i = 1:2:size(lb,1)-2
        rectangle('Position',[lb(i+1) 0 lb(i+2)-lb(i+1) max(top)+75],'FaceColor',[0.9608    0.9608    0.9608],'Edgecolor',[1 1 1])
@@ -50,7 +50,7 @@ for j = 1:genox.number_of_genos
         c = [1 1 1];
     end
     
-plot(time,nanmean(data{j}'),...
+plot(time,mean(data{j}', 'omitnan'),...
     'color', c);     
     
 % % and to update the legend
@@ -59,7 +59,7 @@ end
 
 % determine size for y axis based on larger value
 for i = 1:genox.number_of_genos
-    to_sort(i) = max(nanmean(data{i}'));
+    to_sort(i) = max(mean(data{i}', 'omitnan'));
 end
 axis([time(1) time(end) 0 1.1*max(to_sort)]) % for 10% on biggest value
 
